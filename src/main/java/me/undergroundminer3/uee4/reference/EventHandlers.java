a89d941d48c7bb6aefc75f8419267f80d84f2bd5
undergroundminer3 <undergroundminer3@gmail.com>
2014-04-24 22:16:35 -0400

    The revolution is over!

--- src/main/java/me/undergroundminer3/uee4/reference/EventHandlers.java
+++
@@ -1,8 +0,0 @@
-package me.undergroundminer3.uee4.reference;
-
-import me.undergroundminer3.uee4.handler.ItemEventHandler;
-
-public class EventHandlers
-{
-	public static final ItemEventHandler itemEventHandler = new ItemEventHandler();
-}
--- src/main/java/me/undergroundminer3/uee4/reference/Messages.java
+++
@@ -1,7 +0,0 @@
-package me.undergroundminer3.uee4.reference;
-
-public class Messages
-{
-	public static final String TOOLTIP_INFUSED_WITH = "tooltip.uee4:infusedPrefix";
-	public static final String UPGRADES_CHESTS = "tooltip.uee4:upgradesPrefix";
-}
